e8b23c37fdd8cf92c53d5ae20400f3c8876904f1
bjh26 <bjhuang@umass.edu>
2025-04-25 19:10:39 -0400

    Fixes #50 Fixes #47

--- backend/serverBlair.js
+++ backend/serverBlair.js
@@ -3,7 +3,6 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
-
 app.use(express.static("../frontend/src/NewUserCreation"));
 app.use(express.static("../frontend/src/assets"));
 app.use(express.json());
